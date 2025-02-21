27da78656d55e8b79d94203cf4a4edd6c038a54b
joao.ottoni <jvoo2016@gmail.com>
2025-02-21 20:29:45 -0300

    Criação de modelos

---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/model/Cliente.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
