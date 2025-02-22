fef6152b6e2dc25fd651e86819d7b15228818701
Joao Ottoni <jvoo2016@gmail.com>
2025-02-22 00:13:41 -0300

    Inclusão de novas classes/interfaces

---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/service/ClienteServiceImpl.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.service;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Cliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
